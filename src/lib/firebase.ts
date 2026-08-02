9003942c89f2540e20c922589988a39ef9b53d5d
siw777513-spec <siw777513@gmail.com>
2026-08-02 19:59:03 +0600

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNdp71_i5S8b3RdypvtJQ-IXK9P5vjssU",
+  authDomain: "onefeed-bangladesh.firebaseapp.com",
+  projectId: "onefeed-bangladesh",
+  storageBucket: "onefeed-bangladesh.firebasestorage.app",
+  messagingSenderId: "658176332596",
+  appId: "1:658176332596:web:0101c94065e834c1ff8b61"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
